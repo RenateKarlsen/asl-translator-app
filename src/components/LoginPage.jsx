a276e7259d081c0cbae895a8cf29ecf9db7e7e69
Renate Karlsen <renate.karlsen@no.experis.com>
2022-09-19 15:03:19 +0200

    App now posts user to api

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+import { postUser } from "../services/UserService"
+
 const LoginPage = () => {
-    return (
-        <h1>Hei hallois</h1>
-    )
+  const [userName, setUserName] = useState("")
+
+  const handleNameChange = (event) => {
+    setUserName(event.target.value)
+  }
+
+  const handleRegisterSubmit = (event) => {
+    event.preventDefault()
+    postUser(userName)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleRegisterSubmit}>
+        <fieldset>
+          <input type="text" value={userName} onChange={handleNameChange} />
+          <button type="submit">Create new user</button>
+        </fieldset>
+      </form>
+      <p>Heihei</p>
+    </>
+  )
 }
 
-
-export default LoginPage;
+export default LoginPage
